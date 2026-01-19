32870374e94bda20d080b312ca1805112505559f
Matheus Henrique <matheus.henrique4245@gmail.com>
2026-01-19 08:35:31 -0300

    feat: add .env to config file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,7 @@
 // export const PATH_IMAGES_BUDGET = "http://localhost:5173/budgetCorpImages/";
 // export const PATH_IMAGES = "http://localhost:5173/";
 
-export const API_URL = "http://192.168.10.87:3335";
-export const PATH_IMAGES_BUDGET = "http://192.168.10.87:83/budgetCorpImages/";
-export const PATH_IMAGES = "http://192.168.10.87:83/";
+
+export const API_URL = import.meta.env.VITE_API_URL || "http://192.168.10.87:3335";
+export const PATH_IMAGES_BUDGET = import.meta.env.VITE_PATH_IMAGES_BUDGET || "http://192.168.10.87:83/budgetCorpImages/";
+export const PATH_IMAGES = import.meta.env.VITE_PATH_IMAGES || "http://192.168.10.87:83/";
